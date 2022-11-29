57f207261c8a7592ca42ebbd3492b8a5648ece06
Atharva Ranade <ARanade@nbbj.com>
2022-11-28 16:18:30 -0800

    to-do list

--- jQueryToDoList.js
+++ jQueryToDoList.js
@@ -3,24 +3,34 @@ $(document).ready(function(){
   /**
    * Toggles "done" class on <li> element
   */
-  const doneEvent = function(){
-    $('li').click(function () {
+  const doneEvent = function(li){
+    li.click(function () {
         //toggle "done" class
         $(this).toggleClass(`done`)
     })
   }
-  doneEvent()
+  $('li').each(function(_i, li) {
+    let $item = $(li);
+    doneEvent($item)
+  });
 
+
+  
   /**
    * Delete element when delete link clicked
-  */
-   const deleteEvent = function(){
-    $('.delete').click(function () {
-        //toggle its "done" class
-        $(this).parent().fadeOut()
+   */
+  const deleteEvent = function(btn){
+    btn.click(function () {
+      //toggle its "done" class
+      $(this).parent().fadeOut()
     })
   }
-  deleteEvent()
+  
+  $('.delete').each(function(_i, li) {
+    let $btn = $(li);
+    deleteEvent($btn)
+  });
+
 
   /**
    * Adds new list item to <ul>
@@ -30,30 +40,27 @@ $(document).ready(function(){
     e.preventDefault();
     let $input = $('input')
     const $text = $input.val();
-    const $span = $('<span>')
-      .text($text)
+    const $span = $('<span>').text($text)
 
-    // create delete element.
+    // create delete element
     const $deleteBtn = $('<a>')
       .text('Delete')
       .addClass('delete')
       
-    //new element.
+    //new element
     const $newItem = $('<li>').append($span).append($deleteBtn)
 
     // Place at the end of the UL tag
     $('ul').append($newItem)
 
     // add delete and strikethrough logic to new li
-    doneEvent()
-    deleteEvent()
+    doneEvent($newItem)
+    deleteEvent($deleteBtn)
 
     //clear the text field
-    $input.val = ''
+    $input.val('')
   }
 
   // Add listener
   $('.add-item').click(addListItem)
-
-
 });
